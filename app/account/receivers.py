65e04d79620a8b3507484775c5c3331ac027403d
Olena Bazilo <olena.bazilo@ukr.net>
2023-04-09 16:44:51 +0300

    Homework18

--- app/account/receivers.py
+++ app/account/receivers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 User = get_user_model()
