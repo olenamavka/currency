e941ff0f26f41687f12ea9d9321bfda1b505eaf9
Olena Bazilo <olena.bazilo@ukr.net>
2023-04-17 11:29:53 +0300

    Homework20

--- app/currency/filters.py
+++ app/currency/filters.py
@@ -7,18 +7,29 @@ class RateFilter(django_filters.FilterSet):
 
     class Meta:
         model = Rate
-        fields = ['buy', 'sale']
+        # fields = ['buy', 'sale']
+        fields = {
+            'buy': ('gt', 'gte', 'lt', 'lte', 'exact'),
+            'sale': ('gt', 'gte', 'lt', 'lte', 'exact'),
+        }
 
 
 class ContactUsFilter(django_filters.FilterSet):
 
     class Meta:
         model = ContactUs
-        fields = ['name', 'subject']
+        # fields = ['name', 'subject']
+        fields = {
+            'name': ('contains', 'startswith', 'endswith', 'exact'),
+            'subject': ('contains', 'startswith', 'endswith', 'exact'),
+        }
 
 
 class SourceFilter(django_filters.FilterSet):
 
     class Meta:
         model = Source
-        fields = ['name']
+        # fields = ['name']
+        fields = {
+            'name': ('contains', 'startswith', 'endswith', 'exact'),
+        }
